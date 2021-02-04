532a9153850371fa478829b109e62370ba8d4697
Drew Turner <drewturner500@gmail.com>
2021-02-04 09:01:45 -0800

    Made small change

--- InClassActivity/test_calculator.py
+++ InClassActivity/test_calculator.py
@@ -10,19 +10,19 @@ class TestCalculator(unittest.TestCase):
     def test_subtraction(self):
         self.assertEqual(calculator.calculator_sub(3.5, 1.0), 2.5)
         self.assertEqual(calculator.calculator_sub(1.0, 3.5), -2.5)
-        self.assertEqual(calculator.calculator_add("abs", "jhs"), None)
-        self.assertEqual(calculator.calculator_add(True, False), None)
+        self.assertEqual(calculator.calculator_sub("abs", "jhs"), None)
+        self.assertEqual(calculator.calculator_sub(True, False), None)
 
     def test_multiplication(self):
         self.assertEqual(calculator.calculator_mul(3.5, 3.5), 12.25)
-        self.assertEqual(calculator.calculator_add("abs", "jhs"), None)
-        self.assertEqual(calculator.calculator_add(True, False), None)
+        self.assertEqual(calculator.calculator_mul("abs", "jhs"), None)
+        self.assertEqual(calculator.calculator_mul(True, False), None)
 
     def test_division(self):
         self.assertEqual(calculator.calculator_div(9.0, 4.0), 2.25)
         self.assertEqual(calculator.calculator_div(9.0, 0.0), None)
-        self.assertEqual(calculator.calculator_add("abs", "jhs"), None)
-        self.assertEqual(calculator.calculator_add(True, False), None)
+        self.assertEqual(calculator.calculator_div("abs", "jhs"), None)
+        self.assertEqual(calculator.calculator_div(True, False), None)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
