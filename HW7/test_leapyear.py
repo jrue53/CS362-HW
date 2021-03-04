22258c92304b54c82a3c7d9e28f0255cdd826e22
Drew Turner <drewturner500@gmail.com>
2021-03-04 11:05:40 -0800

    Wrote second test for leap year, tests to see if program can check for years over 100 or not

--- HW7/test_leapyear.py
+++ HW7/test_leapyear.py
@@ -6,5 +6,10 @@ class TestLeapYear(unittest.TestCase):
         self.assertEqual(leapyear.ly(24), "This is a leap year")
         self.assertEqual(leapyear.ly(5), "This is not a leap year")
 
+    def test_hundred(self):
+        self.assertEqual(leapyear.ly(100), "This is a leap year")
+        self.assertEqual(leapyear.ly(103), "This is not a leap year")
+
+
 if __name__ == '__main__':
     unittest.main(verbosity=2)
