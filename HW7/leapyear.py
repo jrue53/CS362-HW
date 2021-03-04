aae2c35546a4600c1467bd4da050cd206fec4bc3
Drew Turner <drewturner500@gmail.com>
2021-03-04 11:01:58 -0800

    Implemented the functionality to test if a year less then 100 is a leapyear or not, test passed

--- HW7/leapyear.py
+++ HW7/leapyear.py
@@ -1,2 +1,7 @@
 def ly(year):
-    
+    leapyear = "This is a leap year"
+    nleapyear = "This is not a leap year"
+    if year % 4 == 0:
+        return leapyear
+    else:
+        return nleapyear
