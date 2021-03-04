ca56e8cfee6c1bac002a283e7760eddcf69225f2
Drew Turner <drewturner500@gmail.com>
2021-03-04 10:52:55 -0800

    Implemented the final functionality for fizzbuzz, the program now prints numbers, fizz, buzz and fizzbuzz appropriately, test passed.

--- HW7/fizzbuzz.py
+++ HW7/fizzbuzz.py
@@ -1,14 +1,16 @@
-  def fb():
+def fb():
     fizzbuzz = ''
     for i in range (1, 101):
-        if i % 3 == 0:
+        if i % 3 == 0 and i % 5 == 0:
+            fizzbuzz += 'fizzbuzz'
+        elif i % 3 == 0 and i % 5 != 0:
             fizzbuzz += 'fizz'
-        elif i % 3 != 0:
+        elif i % 5 == 0 and i % 3 != 0:
+            fizzbuzz += 'buzz'
+        else:
             fizzbuzz += str(i)
+
         if i != 100:
             fizzbuzz += ', '
-    
-    print(fizzbuzz)
-    return fizzbuzz
 
-fb()
+    return fizzbuzz
