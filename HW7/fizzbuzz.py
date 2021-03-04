d2be038339fe5afcc6f155b637dcf29c8142720d
Drew Turner <drewturner500@gmail.com>
2021-03-04 10:34:09 -0800

    Implemented default functionality to print 1-100, with fizz printed for every multiple of 3, passed

--- HW7/fizzbuzz.py
+++ HW7/fizzbuzz.py
@@ -1,8 +1,14 @@
 def fb():
     fizzbuzz = ''
     for i in range (1, 101):
-        fizzbuzz += str(i)
+        if i % 3 == 0:
+            fizzbuzz += 'fizz'
+        elif i % 3 != 0:
+            fizzbuzz += str(i)
         if i != 100:
             fizzbuzz += ', '
-
+    
+    print(fizzbuzz)
     return fizzbuzz
+
+fb()
